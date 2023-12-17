70ed35c6459f9e6c5d259c3fa7bb2a1aa25da3b8
gurb <pcgezer@hotmail.com>
2023-12-17 16:43:50 +0300

    works on project menu

--- draftio/views/sections/ProjectTreeView.axaml.cs
+++ draftio/views/sections/ProjectTreeView.axaml.cs
@@ -2,7 +2,11 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
 using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 using draftio.viewmodels;
+using System;
+using System.Diagnostics.Metrics;
 
 namespace draftio.views.sections
 {
@@ -44,20 +48,59 @@ namespace draftio.views.sections
 
             foreach (var node in ViewModel.Nodes)
             {
-                Rectangle rect = new Rectangle();
-                rect.Height = 35;
-                rect.Fill = Brushes.Aqua;
-                rect.Width = ProjectMenu.Bounds.Width;
-                rect.HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Stretch;
-                Canvas.SetLeft(rect, 0);
-                Canvas.SetTop(rect, counter * 40 + 10);
+                Grid button = MenuButton();
+                Canvas.SetLeft(button, 0);
+                Canvas.SetTop(button, counter * 40 + 10);
 
-
-                ProjectMenu.Children.Add(rect);
+                ProjectMenu.Children.Add(button);
 
                 counter++;
             }
 
         }
+
+
+        private Grid MenuButton()
+        {
+            Grid grid = new Grid();
+
+            try
+            {
+
+                grid.HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Stretch;
+                grid.VerticalAlignment = Avalonia.Layout.VerticalAlignment.Stretch;
+                grid.Height = 30;
+                grid.Background = Brushes.Aqua;
+                grid.Width = ProjectMenu.Bounds.Width;
+                grid.ColumnDefinitions = new ColumnDefinitions("50, 50, *");
+
+
+                Rectangle caret = new Rectangle();
+                caret.Width = 25;
+                caret.Height = 25;
+                caret.Fill = new ImageBrush(new Bitmap(AssetLoader.Open(new Uri("avares://draftio/assets/zoomin.png"))));
+                Grid.SetColumn(caret, 0);
+                grid.Children.Add(caret);
+
+                Image icon = new Image();
+                icon.Source = new Bitmap(AssetLoader.Open(new Uri("avares://draftio/assets/zoomout.png")));
+                Grid.SetColumn(icon, 1);
+                grid.Children.Add(icon);
+
+                TextBlock textBlock = new TextBlock();
+                textBlock.Text = "Text";
+                textBlock.HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Stretch;
+                textBlock.VerticalAlignment = Avalonia.Layout.VerticalAlignment.Stretch;
+                Grid.SetColumn(textBlock, 2);
+                grid.Children.Add(textBlock);
+            }
+            catch (Exception ex) { }
+
+
+            return grid;
+        }
+
+
+        
     }
 }
