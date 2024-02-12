8de4598e64ff47f20f20e8687ddce91c63afddff
gurb <pcgezer@hotmail.com>
2024-02-12 16:16:54 +0300

    version 0.0.4

--- vayfrem/services/VersionControlManager.cs
+++ vayfrem/services/VersionControlManager.cs
@@ -8,6 +8,6 @@ namespace vayfrem.services
 {
     public class VersionControlManager
     {
-        public string? Version { get; set; } = "Pre-Alpha v0.0.3";
+        public string? Version { get; set; } = "Pre-Alpha v0.0.4";
     }
 }
